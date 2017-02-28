b02480fd5cddd5d369c60ea6b7cad5951359e36c
Shantanu V Lad <shantanu.lad@cybergindia.local>
2017-02-28 13:26:44 +0530

    Minor modifications

--- assets/app.js
+++ assets/app.js
@@ -3,40 +3,31 @@ angular.module('app',[
 ])
 
 angular.module('app')
-    .controller('PostsCtrl', ["$scope", "$http", function($scope, $http, ) {
-    	console.log("herer")
-    }])
+.controller('PostsCtrl', function($scope) {
+	$scope.addPost = function() {
+		if ($scope.postBody) {
+			PostsSvc.create ({ username: 'Kirk', body: $scope.postBody}).success(function (post) {
+				$scope.posts.unshift(post)
+				$scope.postBody = null;
+			})
+		}
+	}
 
+	PostsSvc.fetch().success(function (posts) {
+		$scope.posts = posts
+	})
 
-// var app = angular.module('app', [
-// 	'ngRoute'])
-
-// angular.module('app')
-// .controller('PostsCtrl', function($scope) {
-// 	$scope.addPost = function() {
-// 		if ($scope.postBody) {
-// 			PostsSvc.create ({ username: 'Kirk', body: $scope.postBody}).success(function (post) {
-// 				$scope.posts.unshift(post)
-// 				$scope.postBody = null;
-// 			})
-// 		}
-// 	}
-
-// 	PostsSvc.fetch().success(function (posts) {
-// 		$scope.posts = posts
-// 	})
-
-// 	$http.get('/api/posts').success(function (posts) { 
-// 		$scope.posts = posts	
-// 	})
-// 	console.log("In this")
-// }
-// angular.module('app')
-// .service('PostsSvc', function ($http) {
-// 	this.fetch = function () {
-// 		return $http.get('/api/posts')
-// 	}
-// 	this.create = function (post) {
-// 		return	$http.get('/api/posts', post)	
-// 	}
-// })
+	$http.get('/api/posts').success(function (posts) { 
+		$scope.posts = posts	
+	})
+	console.log("In this")
+})
+angular.module('app')
+.service('PostsSvc', function ($http) {
+	this.fetch = function () {
+		return $http.get('/api/posts')
+	}
+	this.create = function (post) {
+		return	$http.get('/api/posts', post)	
+	}
+})
